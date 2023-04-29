3cd149d3650e50fd870698d6952bfcd3d0549501
StoneRen <ren@stone.com>
2023-04-29 20:09:35 +0800

    perf(telescope): 调整快捷键

--- lua/stoneren/config/telescope-config.lua
+++ lua/stoneren/config/telescope-config.lua
@@ -1,11 +1,19 @@
+local telescope = require('telescope')
+local acitons = require('telescope.actions')
+
+telescope.setup {}
+
+telescope.load_extension('fzf')
+
 local bind = vim.keymap.set
 local builtin = require('telescope.builtin')
 bind('n', '<leader>ff', builtin.find_files, {})
+bind('n', '<leader>fs', builtin.grep_string, {})
 bind('n', '<leader>fg', builtin.live_grep, {})
--- bind('n', '<leader>fb', builtin.buffers, {})
+bind('n', '<leader>fb', builtin.buffers, {})
 bind('n', '<leader>fh', builtin.help_tags, {})
 
-require("telescope").load_extension "file_browser"
+telescope.load_extension "file_browser"
 
 bind('n', '<leader>e',":Telescope file_browser path=%:p:h select_buffer=true<CR>", {})
 bind('n', '<leader>te',":tabnew | Telescope file_browser path=%:p:h select_buffer=true<CR>", {})
